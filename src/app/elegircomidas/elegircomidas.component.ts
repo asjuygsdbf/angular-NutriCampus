f45e1a8e764c7a6b60197f1d6822af8166aa6275
DianCarolinee <Dianacs78@hotmail.com>
2024-06-13 10:11:49 -0500

    Elegir comidas agregado2

--- src/app/elegircomidas/elegircomidas.component.ts
+++ src/app/elegircomidas/elegircomidas.component.ts
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class ElegircomidasComponent {
 
+  products = [
+    { id: 'id-1', name: 'Samsung Galaxy Note 4', quantity: 1, price: 2890.66 },
+    { id: 'id-2', name: 'Logitech Keyboard', quantity: 1, price: 120.50 }
+  ];
+  total = 0;
+  selected: string[] = [];
 }
+
