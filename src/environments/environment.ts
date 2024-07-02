53c67ed2d818a9aec7a0df0ba605bbebf9976cec
jmlc643 <josemarialuyocampos@gmail.com>
2024-07-02 13:56:34 -0500

    Registrar integrado

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  urlBack:'https://nutricampus-back.onrender.com',
+  urlMicroservicioDieta: 'https://nutricampus-microservicio-v2.onrender.com',
+  urlFront: 'https://develop--nutricampus.netlify.app',
+  urlMicroservicioRutina: 'https://nutricampus-microservicio-rutina.onrender.com',
+  urlMicroservicioUsuario: 'https://nutricampus-microservicio-usuario.onrender.com'
+};
