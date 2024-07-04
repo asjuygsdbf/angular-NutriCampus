769a2e9a6664a17d92c19584ae39db1eed430883
jmlc643 <josemarialuyocampos@gmail.com>
2024-07-04 18:07:34 -0500

    Drag and Drop añadido

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@ export const environment = {
   urlBack:'https://nutricampus-back.onrender.com',
   urlMicroservicioDieta: 'https://nutricampus-microservicio-v2.onrender.com',
   //urlMicroservicioDieta: 'http://localhost:8081',
-  urlFront: 'https://angular-nutri-campus.vercel.app/registro',
+  urlFront: 'https://angular-nutri-campus.vercel.app',
   //urlFront: 'http://localhost:4200',
   urlMicroservicioRutina: 'https://nutricampus-microservicio-rutina.onrender.com'
   //urlMicroservicioRutina: 'http://localhost:8083'
